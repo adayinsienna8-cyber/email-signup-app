4ce16691629ff8977b2490bd669bbe3344b13baf
dylan-verdant-capital <a.dayinsienna8@gmail.com>
2026-06-20 08:12:14 -0700

    fix deployment

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+
+const User = mongoose.models.User || mongoose.model("User", {
+
+    email:String,
+    password:String
+
+});
+
+
 app.post("/signup", async (req,res)=>{
+
+try{
+
+await mongoose.connect(process.env.MONGO_URL);
+
 
 const newUser = new User({
 
@@ -19,4 +45,26 @@ message:"Premium account created!"
 });
 
 
+}catch(error){
+
+console.log(error);
+
+res.status(500).json({
+
+message:error.message
+
 });
+
+}
+
+});
+
+
+app.get("/", (req,res)=>{
+
+res.sendFile(__dirname + "/public/index.html");
+
+});
+
+
+module.exports = app;
